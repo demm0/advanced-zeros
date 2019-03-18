e76e635e28d5e82148c333c01eb6f8d918fa09c8
demm0 <demm0@mail.ru>
2019-03-18 23:59:40 +0300

    update index.js
    edit code

--- src/index.js
+++ src/index.js
@@ -1,48 +1,32 @@
 module.exports = function getZerosCount(number, base) {
-   // your implementation
-   var primenumbers = [2,	3,	5,	7,	11,	13,	17,	19,	23,	29,	31,	37,	41,	43,	47,	53,	59,	61,	67,	71,	73,	79,	83,	89,	97,
-	101,	103,	107,	109,	113,	127,	131,	137,	139,	149,	151,	157,	163,	167,	173,
-	179,	181,	191,	193,	197,	199,	211,	223,	227,	229,	233,	239,	241,	251,];
-		
-	let result = [];
-	let num = base;
-	let primenumberslength = primenumbers.length;
-	for (let i = 0; i < primenumberslength; i++) {
-		if (num % primenumbers[i] == 0) {
-			result.push([primenumbers[i], 0, 0]);
-				while (num % primenumbers[i] == 0) {
-				result[result.length - 1][1] = result[result.length - 1][1] + 1;
-				num = (num - num % primenumbers[i]) / primenumbers[i];
-			}
-			if (num == 1) {
-				break;
-			}
+	let i = 2;
+	let SI = base;
+	let primeNumbers = [];
+	while ((SI > 0) && (i <= SI)) {
+		while (SI % i == 0) {
+			primeNumbers.push(i);
+			SI = SI / i;
 		}
+		i ++;
 	}
-
-let resultlength = result.length;
-	for (let factItem = number; factItem > 1; factItem--) {
-		for (let i = 0; i < resultlength; i++) {
-			let workFactItem = factItem;
-			let primenumber = result[i][0];
-			if (workFactItem % (primenumber) == 0) {
-				while (workFactItem % (primenumber) == 0) {
-					result[i][2] = result[i][2] + 1;
-					workFactItem = (workFactItem - workFactItem % primenumber) / primenumber;	
-				}
-				if(workFactItem == 1) {
-					break;
-				}
-			}
+  	let basePrimeNumbers = [];
+  	primeNumbers.map (( x , index )=>{
+		if ((primeNumbers[index - 1]) != x || (index == 0)){
+  			basePrimeNumbers.push([x,1]);
 		}
-	}
-	
-	let zeroCount = (result[0][2] - result[0][2] % result[0][1]) / result[0][1];
-	for (let i = 0; i < resultlength; i++) {
-		let vector = result[i];
-		if (zeroCount > (vector[2] - vector[2] % vector[1]) / vector[1]){
-			zeroCount = (vector[2] - vector[2] % vector[1]) / vector[1];
+		else {
+			basePrimeNumbers[basePrimeNumbers.length - 1][1] ++;
 		}
-	}
-	return zeroCount;
+  	});
+ 	let basePrimeNumbersInfo = [];
+ 	basePrimeNumbers.map ((x) =>{
+ 		let num = number;
+ 		let zerocount = 0;
+ 		while ( num > 0 ){
+ 			num = Math.floor(num / x[0]);
+ 			zerocount = num + zerocount;
+ 		}
+ 		basePrimeNumbersInfo.push(Math.floor(zerocount/x[1]));
+ 	});
+ 	return Math.min(...basePrimeNumbersInfo);
 }
